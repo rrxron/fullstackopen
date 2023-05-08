5f9f1b60346ea5d2bb4f1f7d81e5d4e10a081364
rrxron <128441622+rrxron@users.noreply.github.com>
2023-05-08 17:52:18 +0800

    part1, 1.7: unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,17 +4,61 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+  const [feedbacks, setFeedbacks] = useState([])
+
+  const updatePositive = (feedbackValue) => {
+    if (feedbackValue.length > 0) {
+      const goodOnly = feedbackValue.filter((i) => i === 'G')
+      setPositive((goodOnly.length / feedbackValue.length) * 100)
+    }
+  }
+
+  const updateAverage = (latestGood, latestBad, currentFeedbacks) => {
+    const finalScore = latestGood - latestBad
+    setAverage(((finalScore / currentFeedbacks.length) * 100) / 100)
+  }
+
+  const goodHandler = () => {
+    const updatedValue = good + 1
+    setGood(updatedValue)
+    const feedbackValue = feedbacks.concat('G')
+    setFeedbacks(feedbackValue)
+    updatePositive(feedbackValue)
+    updateAverage(updatedValue, bad, feedbackValue)
+  }
+
+  const neutralHandler = () => {
+    const updatedValue = neutral + 1
+    setNeutral(updatedValue)
+    const feedbackValue = feedbacks.concat('N')
+    setFeedbacks(feedbackValue)
+    updatePositive(feedbackValue)
+  }
+
+  const badHandler = () => {
+    const updatedValue = bad + 1
+    setBad(updatedValue)
+    const feedbackValue = feedbacks.concat('B')
+    setFeedbacks(feedbackValue)
+    updatePositive(feedbackValue)
+    updateAverage(good, updatedValue, feedbackValue)
+  }
 
   return (
     <>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <button onClick={goodHandler}>good</button>
+      <button onClick={neutralHandler}>neutral</button>
+      <button onClick={badHandler}>bad</button>
       <h1>statistics</h1>
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {feedbacks.length}</div>
+      <div>average {average}</div>
+      <div>positive {positive}%</div>
     </>
   )
 }
