3a0838ece09d02b97dc045d0249c54019b64d856
rrxron <128441622+rrxron@users.noreply.github.com>
2023-05-08 14:51:57 +0800

    part1, c. Component -> Refactoring the components

--- _notes/1/part1/src/App.js
+++ _notes/1/part1/src/App.js
@@ -1,19 +1,34 @@
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age
+import { useState } from 'react'
+import Display from './Display'
+import Button from './Button'
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
 
   return (
     <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
+      <Display counter={counter} />
+      <Button handleClick={increaseByOne} text="plus" />
+      <Button handleClick={setToZero} text="zero" />
+      <Button handleClick={decreaseByOne} text="minus" />
     </div>
   )
-}
-
-const App = (props) => {
-  const { counter } = props
-  return <div>{counter}</div>
 }
 
 export default App
