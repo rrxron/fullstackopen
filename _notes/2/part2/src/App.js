b6bd8b93179e787bfc56d3e1b65784f36d4bafdf
rrxron <128441622+rrxron@users.noreply.github.com>
2023-05-09 18:50:20 +0800

    part2, Rendering -> Web developer's oath

---
+++ _notes/2/part2/src/App.js
@@ -0,0 +1,16 @@
+import Note from './components/Note'
+
+const App = ({ notes }) => {
+  return (
+    <>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+    </>
+  )
+}
+
+export default App
---
+++ _notes/2/part2/src/components/Note.js
@@ -0,0 +1,3 @@
+const Note = ({ note }) => <li key={note.key}>{note.content}</li>
+
+export default Note
