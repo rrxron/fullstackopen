1c9b70ac4a22e223a0061e57fcd2f2c8ce9cc56d
rrxron <128441622+rrxron@users.noreply.github.com>
2023-05-11 22:36:29 +0800

    part2, 2.9: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' },
+    { name: 'Arto Hellas', number: '040-1234567', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterValue, setFilterValue] = useState('')
 
   const handleForm = (e) => {
     e.preventDefault()
@@ -24,12 +28,24 @@ const App = () => {
   }
 
   const handleNameChange = (e) => setNewName(e.target.value)
-
   const handleNumberChange = (e) => setNewNumber(e.target.value)
+  const handleFilterChange = (e) => setFilterValue(e.target.value)
+
+  const filtered_person =
+    filterValue === ''
+      ? persons
+      : persons.filter((person) =>
+          person.name.toLowerCase().startsWith(filterValue.toLowerCase())
+        )
 
   return (
     <>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with{' '}
+        <input value={filterValue} onChange={handleFilterChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleForm}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -40,15 +56,17 @@ const App = () => {
         <div>
           <button type="submit">add</button>
         </div>
-        <h2>Numbers</h2>
-        <div>
-          {persons.map(({ name, number }) => (
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {filtered_person.map(({ name, number }) => {
+          return (
             <div key={name}>
               {name} {number}
             </div>
-          ))}
-        </div>
-      </form>
+          )
+        })}
+      </div>
     </>
   )
 }
