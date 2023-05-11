3c9eab519a160095fccf9866e0cc5e6145094186
rrxron <128441622+rrxron@users.noreply.github.com>
2023-05-11 22:25:57 +0800

    part2, 2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-1234567' },
+  ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleForm = (e) => {
     e.preventDefault()
@@ -12,15 +15,17 @@ const App = () => {
       setPersons(
         persons.concat({
           name: newName,
+          number: newNumber,
         })
       )
       setNewName('')
+      setNewNumber('')
     }
   }
 
-  const handleNameChange = (e) => {
-    setNewName(e.target.value)
-  }
+  const handleNameChange = (e) => setNewName(e.target.value)
+
+  const handleNumberChange = (e) => setNewNumber(e.target.value)
 
   return (
     <>
@@ -30,12 +35,17 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
         <h2>Numbers</h2>
         <div>
-          {persons.map(({ name }) => (
-            <div key={name}>{name}</div>
+          {persons.map(({ name, number }) => (
+            <div key={name}>
+              {name} {number}
+            </div>
           ))}
         </div>
       </form>
