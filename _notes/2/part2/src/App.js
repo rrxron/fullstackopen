723d8634169ccf743afe53c3c19af883939acaf8
rrxron <128441622+rrxron@users.noreply.github.com>
2023-05-22 13:57:58 +0800

    part2, Altering data -> Changing the Importance of Notes

--- _notes/2/part2/src/App.js
+++ _notes/2/part2/src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+
 import Note from './components/Note'
 
 const App = () => {
@@ -17,15 +18,27 @@ const App = () => {
 
   useEffect(hook, [])
 
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find((n) => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    axios.put(url, changedNote).then((response) => {
+      setNotes(notes.map((n) => (n.id !== id ? n : response.data)))
+    })
+  }
+
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
-      id: notes.length + 1,
     }
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+
+    axios.post('http://localhost:3001/notes', noteObject).then((response) => {
+      setNotes(notes.concat(response.data))
+      setNewNote('')
+    })
   }
 
   const handleNoteChange = (event) => {
@@ -45,7 +58,11 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>
